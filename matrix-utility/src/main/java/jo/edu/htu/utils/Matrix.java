84522a46647e10d5dd342ea635a9c65cb1f20ba9
HakamMadi <HakamMadi@github.com>
2020-03-28 09:26:38 +0300

    Matrix utility resolve

--- matrix-utility/src/main/java/jo/edu/htu/utils/Matrix.java
+++ matrix-utility/src/main/java/jo/edu/htu/utils/Matrix.java
@@ -1,33 +1,55 @@
 package jo.edu.htu.utils;
 
 public class Matrix {
+    int rows;
+    int cols;
+    int[][] matrix;
+
     public Matrix(int[][] matrix) {
         for (int row = 0; row < matrix.length; row++) {
-            if (matrix[row] == null)
-                throw new IllegalArgumentException("row " + row + " is null");
-            if (matrix[row].length != matrix[0].length)
+            if (matrix[row] == null) {
+                throw new IllegalArgumentException
+                        ("row " + row + " is null");
+            }
+            if (matrix[row].length != matrix[0].length) {
                 throw new IllegalArgumentException("Inconsistent rows");
+            }
         }
+
+        int[][] myInt = new int[matrix.length][];
+        for (int i = 0; i < matrix.length; i++)
+            myInt[i] = matrix[i].clone();
+
+        this.matrix = myInt;
+        rows = matrix.length;
+        cols = matrix[0].length;
+
     }
 
     public int rows() {
-        return -1;
+        return rows;
     }
 
     public int cols() {
-        return -1;
+        return cols;
     }
 
     public int value(int row, int col) {
-        return 0;
+        if (row > matrix.length - 1) {
+            throw new IllegalArgumentException("invalid cell index: " + row + "," + col);
+        }
+        return matrix[row][col];
     }
 
     public void forEach(ValueConsumer valueConsumer) {
-
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                valueConsumer.accept(row, col, value(row, col));
+            }
+        }
     }
 
     public interface ValueConsumer {
-
         void accept(int row, int col, int value);
     }
 }
